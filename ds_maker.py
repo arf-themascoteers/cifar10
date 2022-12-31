e9bab41519fbc740c9e746383c2503b8d470d523
Arif <80994072+arf-themascoteers@users.noreply.github.com>
2022-12-31 11:30:12 +1100

    done

--- ds_maker.py
+++ ds_maker.py
@@ -8,9 +8,12 @@ torch.manual_seed(0)
 transform = transforms.Compose([
     transforms.ToTensor(),
 ])
-root_dir = "data/shifted/orig"
-dataset = datasets.CIFAR10(root="./data", train=True, download=True)
+
 counter = np.zeros(10, dtype=np.int32)
+
+root_dir = "data/shifted/test"
+dataset = datasets.CIFAR10(root="./data", train=True, download=True)
+
 for index, data in enumerate(dataset):
     x = data[0]
     y = data[1]
@@ -29,6 +32,31 @@ for index, data in enumerate(dataset):
     # image[:,:,0:-1] = image[:,:,1:]
     # image[:,:,-1] = first_col
     Image.fromarray(image).save(location)
-    if index == 100:
-        break
+    if index%1000 == 0:
+        print(f"Done {index}")
 
+dataset = datasets.CIFAR10(root="./data", train=False, download=True)
+
+for index, data in enumerate(dataset):
+    x = data[0]
+    y = data[1]
+    count_item = counter[y]
+    name = f"{y}_{count_item}.png"
+    location = os.path.join(root_dir, name)
+    counter[y] = count_item + 1
+
+    image_orig = np.asarray(x)
+    image = np.copy(image_orig)
+
+    # first_row = np.copy(image[:,0,:])
+    # image[:,0:-1,:] = image[:,1:,:]
+    # image[:,-1,:] = first_row
+    # first_col = np.copy(image[:,:,0])
+    # image[:,:,0:-1] = image[:,:,1:]
+    # image[:,:,-1] = first_col
+    Image.fromarray(image).save(location)
+    if index%1000 == 0:
+        print(f"Done {index}")
+
+
+print("All done")
